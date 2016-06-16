bbbd15162041d444f1a4c5169eaa3f140b393774
Syed Sheeraz Zaidi <zaidi_sheeraz@hotmail.com>
2016-06-16 11:19:43 +0530

    Notes Program Updated

--- Notes.java
+++ Notes.java
@@ -11,25 +11,79 @@ cal(amt,val);
 }
 private static void cal(int amt,int val)
 {
-	int n=0;
+	int n=0,rem=0;
 	if(amt%val==0)
 	{
 		n=amt/val;
 		System.out.println("Number of notes of value"+val+"is="+n);
 	}
 	else {
-		int rem=amt%val;
-		n=amt/val;
-		System.out.print("Number of notes of value"+val+"is="+n);	
-		val=rem;
-		if (val>=100 && val <500) {
-			while (val!=0)
-			{
-						int m=val;
-						int x=m/val;
-						System.out.print(" And Number of notes of value "+val+"is="+x);	
-			}
+		 rem=amt%val;
+		 n=amt/val;
+		 System.out.println("Number of notes of value"+val+" is="+n);	
 		
+		while(rem!=0)
+		{
+			if(rem<1000 && rem>=500)
+			{
+				n=rem/500;
+				rem=rem%500;
+				
+				//count++;
+				System.out.println(" And Number of notes of value 500 is="+n);	
+		// 	}
+			}
+			if(rem<500 && rem>=100)
+			{
+				n=rem/100;
+				rem=rem%100;
+				
+				//count++;
+				System.out.println(" And Number of notes of value 100 is="+n);	
+		// 	}
+			}
+			if (rem>=50 && rem<100)
+			{
+				n=rem/50;
+				rem=rem%50;	
+				
+				System.out.println(" And Number of notes of value 50 is="+n);
+			}
+			if (rem>=20 && rem<50)
+			{
+				n=rem/20;
+				rem=rem%20;	
+				
+				System.out.println(" And Number of notes of value 20 is="+n);
+			}
+			if (rem>=10 && rem<20)
+			{
+				n=rem/10;
+
+				rem=rem%10;	
+				System.out.println(" And Number of notes of value 10 is="+n);
+			}
+			if (rem>=5 && rem<10)
+			{
+				n=rem/5;
+
+				rem=rem%5;	
+				System.out.println(" And Number of notes of value 5 is="+n);
+			}
+			if (rem>=2 && rem<5)
+			{
+				n=rem/2;
+
+				rem=rem%2;	
+				System.out.println(" And Number of notes of value 2 is="+n);
+			}
+			if (rem>=1 && rem<2)
+			{
+				n=rem/1;
+
+				rem=rem%1;	
+				System.out.println(" And Number of notes of value 1 is="+n);
+			}
 		}
 	
 
